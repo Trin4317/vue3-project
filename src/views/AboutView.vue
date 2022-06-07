e54af23a715ef6e3e4c354ecf8fd8b790990ffc0
Trin4317 <eg.artourbabaev@gmail.com>
2022-06-08 01:29:19 +0900

    update Home and About view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,16 @@
+<script>
+// using mixins	
+import flash from "@/mixins/flash";
+
+export default {
+	mixins: [flash]
+}
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <p>
+    	<button @click="flash('Oh hey again!')">Click me</button>
+    </p>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <script setup>
-import TheWelcome from "@/components/TheWelcome.vue";
+// using composables
+import { useFlash } from "@/composables/useFlash";
+
+let { flash } = useFlash();
+
+// without using setup macro
+// export default {
+// 	setup() {
+// 		let { flash } = useFlash();
+
+// 		return { flash };
+// 	}
+// }
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <p>
+    	<button @click="flash('Oh hey!')">Click me</button>
+    </p>
   </main>
 </template>
