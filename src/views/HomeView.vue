efcea40ec1a99e4fd8cb7a023da8a554d0218a97
Trin4317 <eg.artourbabaev@gmail.com>
2022-06-08 02:25:13 +0900

    update home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,47 @@
 <script setup>
-// using composables
-import { useFlash } from "@/composables/useFlash";
+	import { ref, watch } from "vue";
+	import { useStorage } from "@/composables/useStorage";
 
-let { flash } = useFlash();
+	let food = useStorage('food', 'salad');
 
-// without using setup macro
-// export default {
-// 	setup() {
-// 		let { flash } = useFlash();
+	let age = useStorage('age');
 
-// 		return { flash };
-// 	}
-// }
+	let obj = useStorage('obj', { one: 'one'});
+
+	setTimeout(() => {
+		obj.value.one = 'changed';
+	}, 3000);
+
+	// inline script
+	// let food = ref(localStorage.getItem('food'));
+	// let age = ref(localStorage.getItem('age'));
+
+	// watch(food, (val) => {
+	// 	write('food', val);
+	// });
+
+	// watch(age, (val) => {
+	// 	write('age', val);
+	// });
+
+	// function write(key, val) {
+	// 	localStorage.setItem(key, val);
+	// }
+
+	// setTimeout(() => {
+	// 	food.value = 'shrimp';
+	// }, 2000);
 
 </script>
 
 <template>
   <main>
     <p>
-    	<button @click="flash('Oh hey!')">Click me</button>
+    	What is your favorite foods? <input type="text" v-model="food">
+    </p>
+
+    <p>
+    	How old are you? <input type="text" v-model="age">
     </p>
   </main>
 </template>
