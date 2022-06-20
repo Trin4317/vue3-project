eb229475149dba5bc43b3393840e212ead2d5103
Trin4317 <eg.artourbabaev@gmail.com>
2022-06-20 19:22:56 +0900

    add a textarea that allows indentation by Tab key

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,47 +1,37 @@
 <script setup>
-	import { ref, watch } from "vue";
-	import { useStorage } from "@/composables/useStorage";
+import { ref, onMounted } from "vue";
 
-	let food = useStorage('food', 'salad');
+let textarea = ref(null);
 
-	let age = useStorage('age');
+onMounted(() => {
+	textarea.value.addEventListener("keydown", (event) => {
+		let t = textarea.value;
 
-	let obj = useStorage('obj', { one: 'one'});
+		// tab was pressed
+		if (event.keyCode === 9) {
+			// get caret position/selection
+			let val = t.value,
+				start = t.selectionStart,
+				end = t.selectionEnd;
 
-	setTimeout(() => {
-		obj.value.one = 'changed';
-	}, 3000);
+			// set textarea value to: text before caret + tab + text after caret
+			t.value = val.substring(0, start) + "\t" + val.substring(end);
 
-	// inline script
-	// let food = ref(localStorage.getItem('food'));
-	// let age = ref(localStorage.getItem('age'));
+			// put caret at right position again
+			t.selectionStart = t.selectionEnd = start + 1;
 
-	// watch(food, (val) => {
-	// 	write('food', val);
-	// });
+			event.preventDefault();
+		}
+	})	
+})
 
-	// watch(age, (val) => {
-	// 	write('age', val);
-	// });
-
-	// function write(key, val) {
-	// 	localStorage.setItem(key, val);
-	// }
-
-	// setTimeout(() => {
-	// 	food.value = 'shrimp';
-	// }, 2000);
 
 </script>
 
 <template>
   <main>
-    <p>
-    	What is your favorite foods? <input type="text" v-model="food">
-    </p>
-
-    <p>
-    	How old are you? <input type="text" v-model="age">
-    </p>
+    <form>
+    	<textarea ref="textarea" style="width: 100%; height: 300px;">Hi threre</textarea>
+    </form>
   </main>
 </template>
