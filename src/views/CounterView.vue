bddfccaae5ded3070fcef26e2a5ac1ff6eeeff3e
Trin4317 <eg.artourbabaev@gmail.com>
2022-08-24 01:21:38 +0900

    update CounterView to refer to the data from counterStore

--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -1,11 +1,11 @@
-<script>
-
+<script setup>
+import { counter } from "@/stores/counterStore"
 </script>
 
 <template>
   <div>
-    <p>
-    	Counter View
-    </p>
+    <h1>{{ counter.count }}</h1>
+
+    <button @click="counter.count++">Increment</button>
   </div>
 </template>
