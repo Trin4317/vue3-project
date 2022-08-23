a6c863944fd3bdd2150210bd54b40eb02bcd8e23
Trin4317 <eg.artourbabaev@gmail.com>
2022-08-24 01:03:34 +0900

    change state to become reactive

--- src/components/Quiz/QuizFooterLinks.vue
+++ src/components/Quiz/QuizFooterLinks.vue
@@ -6,6 +6,7 @@ import { state } from "@/stores/quizStore";
 <template>
 	<div style="margin-top: 10px;">
 		<h5>{{ state.name }}</h5>
+		<button @click="state.name ='A new name'">Change Quiz Name</button>
 		<ul>
 			<li><a href="#">Get a Job</a></li>
 			<li><a href="#">Contact Us</a></li>
