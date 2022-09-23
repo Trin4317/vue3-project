92e53b7c7cd8d90c07fb113ee0469525c77ae201
Trin4317 <eg.artourbabaev@gmail.com>
2022-09-24 05:50:35 +0900

    create simple static team dashboard page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import CounterView from "../views/CounterView.vue";
+import TeamView from "../views/TeamView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +9,7 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView,
+      component: TeamView,
     },
     {
       path: "/about",
